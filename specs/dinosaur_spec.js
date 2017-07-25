34b505467af330c37e74118c13017dd0b6606c60
vixplows <vicky@vickyplows.co.uk>
2017-07-25 19:47:57 +0100

    Initial commit

---
+++ specs/dinosaur_spec.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var Dinosaur = require('../dinosaur.js');
+
+describe("Dinosaur", function(){
+
+  var dinosaur;
+
+  beforeEach(function(){
+    dinosaur = new Dinosaur();
+  });
+
+  it("should have a type")
+
+});
---
+++ specs/park_spec.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var Dinosaur = require('../dinosaur.js');
+var Park = require('../park.js');
+
+describe("Park", function(){
+
+  var dinosaur;
+  var park;
+
+  beforeEach(function(){
+    dinosaur = new Dinosaur();
+    park = new Park();
+  });
+
+});
